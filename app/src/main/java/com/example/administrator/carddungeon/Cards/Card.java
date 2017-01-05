d449e4f0f8b100a36daf7da75b2f14281d612d60
Max Ma <mashuoyi111@sina.com>
2017-01-04 21:22:37 -0800

    see if commit works

--- app/src/main/java/com/example/administrator/carddungeon/Cards/Card.java
+++ app/src/main/java/com/example/administrator/carddungeon/Cards/Card.java
@@ -3,6 +3,7 @@ package com.example.administrator.carddungeon.Cards;
 /**
  * Created by Administrator on 2017/1/4.
  */
+// a card on board
 public abstract class Card {
     boolean attackable;
     boolean movable;
